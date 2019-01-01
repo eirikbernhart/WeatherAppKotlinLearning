5d1e031fe10529f5c1f75b4fba5472a079b7386b
Eirik Bernhart <eirik@beiningbogen.no>
2019-01-01 23:23:58 +0100

    commit chp 12

--- app/src/main/java/com/wheaterapp/beiningbogen/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/wheaterapp/beiningbogen/weatherapp/domain/model/DomainClasses.kt
@@ -1,5 +1,11 @@
 package com.wheaterapp.beiningbogen.weatherapp.domain.model
 
-data class ForecastList(val city: String, val contry: String, val dailyForecast:List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast:List<Forecast>) {
 
-data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
+    val size: Int get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+
+}
+
+data class Forecast(val date: String, val description: String, val high: Int, val low: Int, val iconUrl: String)
